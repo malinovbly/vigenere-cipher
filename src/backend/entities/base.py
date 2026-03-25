c777d01c5b2c20fceee619239f7e85e2df804851
malinovbly <temanov0530@gmail.com>
2026-03-26 00:54:40 +0500

    + class Cipher; class Base changes

--- src/backend/entities/base.py
+++ src/backend/entities/base.py
@@ -7,14 +7,14 @@ class Base:
                  value: str,
                  lang: Literal['ru', 'en'] = 'ru') -> None:
         self._langs = {
-            'en': self.__contains_only_en_letters,
-            'ru': self.__contains_only_ru_letters
+            'en': self._contains_only_en_letters,
+            'ru': self._contains_only_ru_letters
         }
         if lang not in self._langs.keys():
-            raise ValueError(f'Language must be one of [{', '.join(self._langs.keys())}]')
+            raise ValueError(f'language must be one of [{', '.join(self._langs.keys())}]')
         value = value.lower()
-        if self.__contains_only_letters(value) and self._langs[lang](value):
-            value = self.__replace_specific_letter(value)
+        if self._contains_only_letters(value) and self._langs[lang](value):
+            value = self._replace_specific_letter(value)
             self._value = value
             self._lang = lang
 
@@ -23,39 +23,39 @@ class Base:
         return self._value
 
     @property
-    def language(self) -> str:
+    def language(self) -> Literal['ru', 'en']:
         return self._lang
 
     @staticmethod
-    def __contains_only_letters(value) -> bool:
+    def _contains_only_letters(value) -> bool:
         if not isinstance(value, str):
-            raise TypeError('Value must be a string')
+            raise TypeError('value must be a string')
         if value is None or len(value) < 1:
-            raise ValueError('Value must not be empty')
+            raise ValueError('value must not be empty')
         pattern = r'[^a-zа-яё]'
         clean_key = re.sub(pattern, '', value)
         if clean_key != value:
-            raise ValueError('Value must only have letters')
+            raise ValueError('value must only have letters')
         return True
 
     @staticmethod
-    def __contains_only_en_letters(value) -> bool:
+    def _contains_only_en_letters(value) -> bool:
         pattern = r'[^a-z]'
         clean_key = re.sub(pattern, '', value)
         if clean_key != value:
-            raise ValueError('Value must only have EN letters')
+            raise ValueError('value must only have EN letters')
         return True
 
     @staticmethod
-    def __contains_only_ru_letters(value) -> bool:
+    def _contains_only_ru_letters(value) -> bool:
         pattern = r'[^а-яё]'
         clean_key = re.sub(pattern, '', value)
         if clean_key != value:
-            raise ValueError('Value must only have RU letters')
+            raise ValueError('value must only have RU letters')
         return True
 
     @staticmethod
-    def __replace_specific_letter(value) -> str:
+    def _replace_specific_letter(value) -> str:
         letter_from = 'ё'
         letter_to = 'е'
         return re.sub(letter_from, letter_to, value)
---
+++ src/backend/entities/cipher.py
@@ -0,0 +1,94 @@
+from typing import Literal
+
+from .key import Key
+from .message import Message
+
+
+class Cipher:
+
+    _ALPHABETS = {
+        'ru': 'абвгдежзийклмнопрстуфхцчшщъыьэюя',
+        'en': 'abcdefghijklmnopqrstuvwxyz'
+    }
+
+    def __init__(self,
+                 message: Message,
+                 key: Key,
+                 action: Literal['encrypt', 'decrypt'] = 'encrypt') -> None:
+        self._actions = {
+            'encrypt': self._encrypt,
+            'decrypt': self._decrypt
+        }
+        if action not in self._actions:
+            raise ValueError(f'action must be one of [{', '.join(self._actions.keys())}]')
+        if not isinstance(key, Key):
+            raise TypeError('key must be of type Key')
+        if not isinstance(message, Message):
+            raise TypeError('message must be of type Message')
+        if not (key.language == message.language):
+            raise ValueError('key and message must be of the same language')
+        if len(key.value) > len(message.value):
+            raise ValueError('key must be less than or equal to message')
+        self._message = message
+        self._key = key
+        self._expanded_key = self._expand_key(key, len(message.value))
+        self._lang = key.language
+        self._value = self._actions[action]()
+
+    @property
+    def message(self) -> Message:
+        return self._message
+
+    @property
+    def key(self) -> Key:
+        return self._key
+
+    @property
+    def expanded_key(self) -> Key:
+        return self._expanded_key
+
+    @property
+    def language(self) -> Literal['ru', 'en']:
+        return self._lang
+
+    @property
+    def value(self) -> str:
+        return self._value
+
+    def _encrypt(self) -> str:
+        result = list()
+        msg = self._message.value
+        key = self._expanded_key.value
+        alphabet = self._get_alphabet(self._lang)
+        alphabet_len = len(alphabet)
+        char_to_idx = {char: i for i, char in enumerate(alphabet)}
+        for m_char, k_char in zip(msg, key):
+            m_idx = char_to_idx.get(m_char)
+            k_idx = char_to_idx.get(k_char)
+            new_idx = (m_idx + k_idx) % alphabet_len
+            result.append(alphabet[new_idx])
+        return ''.join(result)
+
+    def _decrypt(self) -> str:
+        pass
+
+    @staticmethod
+    def _expand_key(key: Key, message_len: int) -> Key:
+        """
+        Increase the key length to the given message length by repetition.
+        Args:
+            key (Key): The key to increase.
+            message_len (int): The message length.
+        Returns:
+            Key: The increased key.
+        """
+        key_value = key.value
+        key_len = len(key_value)
+        capacity = message_len // key_len
+        rest = message_len % key_len
+        new_key = key_value * capacity + key_value[:rest]
+        return Key(new_key, lang=key.language)
+
+    @staticmethod
+    def _get_alphabet(language: Literal['ru', 'en']) -> str:
+        return Cipher._ALPHABETS.get(language)
